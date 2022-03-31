1900a60f5c58f18b63c76bdf6c179929960e20d8
Antoine Carnec <antoinecarnec@gmail.com>
2022-03-31 18:23:52 +0200

    Visualisation for Leon

--- code/SBM.R
+++ code/SBM.R
@@ -30,8 +30,6 @@ split_into_k <- function(n, k) {
 # NOTE: There is a paper "Adapting the Stochastic Block Model to Edge-Weighted Networks by Aicher et Al"
 # which discusses estimation on these models, could be useful for later.
 
-
-
 # Generic SBM Sampler
 undirected_asymetric_sbm <- function(n, pref.matrix, block.sizes, sampler=rlnorm) {
     # Sampler is a function that generates the value of the weight.
@@ -55,14 +53,36 @@ create_ER <- function(n, p) {
 }
 
 create_one_big_cluster_graph <- function(n, sampler) {
-    cluster_self_prob <- 0.8
-    cluster_out_prob <- 0.3
-    cluster_in_prob <- 0.6
-    non_cluster_between <- 0.01
+    cluster_self_prob <- 0.7
+    # from big cluster out
+    cluster_out_prob <- 0.2
+    # from big cluster to in
+    cluster_in_prob <- 0.1
+    non_cluster_between <- 0.05
     
-    sbm <- matrix( c( cluster_in_prob, cluster_out_prob, cluster_in_prob, non_cluster_between ), 2, 2 )
-    undirected_asymetric_sbm(n, sbm, c(25, 15), sampler)
+    sbm <- matrix( c( 
+                     cluster_self_prob, cluster_out_prob, 
+                     cluster_in_prob, non_cluster_between), 
+                  nrow=2, ncol=2 )
+    undirected_asymetric_sbm(n, sbm, c(15, 35), sampler)
 }
+
+# TESTING
+graph <- create_one_big_cluster_graph(MAXIMUM_GRAPH_SIZE, sampler=runif)
+graph <- graph %>% activate(nodes) %>% mutate(id = row_number()) %>% mutate(cluster = ifelse( id <= 15, "dense", "sparse"))
+ggraph(graph) + 
+    geom_edge_link(color="black", arrow= arrow(length = unit(4, 'mm'))) + 
+    geom_node_point(aes(color=factor(cluster)), size=4) +
+    theme_void()
+# geom_node_text(aes_string(label=label), size=7, col="black") +
+# scale_x_reverse() +
+# ggtitle(message) +
+# labs(color="groups", title=message) +
+# theme_void() +
+# theme(plot.title = element_text(lineheight=1.5, size=20, face="bold.italic"),
+#       legend.position = "none"
+#       ) +
+# scale_color_tableau("Superfishel Stone")
 
 create_clusters <- function(n, k) {
     # TODO figure out how we want to do the clusters
@@ -71,19 +91,6 @@ create_clusters <- function(n, k) {
     # n: number of nodes 
     ...
 }
-
-# graph_metrics <- function(graph) {
-#     return( list(
-#                 eigenvector_centrality=eigen_centrality(graph)$value,
-#                 meandegree=mean(degree(graph)),
-#                 edges=gsize(graph),
-#                 density=graph.density(graph),
-#                 betweeness=graph.betweeness(graph)
-#      )
-#    ) 
-# }
-
-sample_pa()
 
 regularize_graph <- function(graph) {
     n <- gorder(graph)
@@ -110,12 +117,13 @@ simulate <- function() {
 
     # ATTENTION: every function must take two arguments
     graph_simulators <- list( 
-                             "ER" = function(n, p) create_ER(n, p)
-                             # "1C" = function(n, sampler) create_one_big_cluster_graph()
+                             "ER" = function(n, p) create_ER(n, p),
+                             "1C" = function(n, sampler) create_one_big_cluster_graph(n, sampler)
                              )
 
     param_grid <- list(
-                       "ER"=seq(0, 1, 0.05)[-1]
+                       "ER"=seq(0, 1, 0.05)[-1],
+                       "1C"=runif # ignore
                      )
 
     i <- 1
@@ -129,13 +137,14 @@ simulate <- function() {
                 graph_index=i,
                 graph_simulator=paste(j, p, sep="_"),
                 node_index=1:gorder(graph),
-                eigenvector_centrality=eigen_centrality(graph)$value,
+                eigenvector_centrality=eigen_centrality(graph)$vector,
                 meandegree=mean(degree(graph)),
                 edges=gsize(graph),
                 density=graph.density(graph),
                 betweeness=betweenness(graph),
                 node_to_network=control_metrics[[1]], # tr(W)
-                network_to_node=control_metrics[[2]] # tr(M)
+                network_to_node=control_metrics[[2]], # tr(M)
+                ratio = node_to_network / network_to_node
             )
             print(j)
             if (i == 1) {
@@ -150,3 +159,4 @@ simulate <- function() {
     return(result_df)
 }
 
+simulate()
